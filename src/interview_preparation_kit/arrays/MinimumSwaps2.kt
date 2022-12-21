198716234d57c2a5047b6aa8123a598431e0b9a9
alex <arcolistro@gmail.com>
2022-12-21 00:46:27 -0800

    Update solution file with youtube resource

--- src/interview_preparation_kit/arrays/MinimumSwaps2.kt
+++ src/interview_preparation_kit/arrays/MinimumSwaps2.kt
@@ -6,6 +6,8 @@ package interview_preparation_kit.arrays
  * You are given an unordered array consisting of consecutive integers
  * [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements.
  * Find the minimum number of swaps required to sort the array in ascending order.
+ *
+ * Explanation resource used: https://youtu.be/Def9kMtZCNA
  */
 
 import java.io.*
