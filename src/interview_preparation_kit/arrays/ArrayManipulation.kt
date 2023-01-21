1671fe66721b8a7bdc1a5a3d1f666c9193066660
alex <arcolistro@gmail.com>
2023-01-20 20:56:43 -0800

    Add array manipulation file with solution

--- src/interview_preparation_kit/arrays/ArrayManipulation.kt
+++ src/interview_preparation_kit/arrays/ArrayManipulation.kt
@@ -1,21 +1,11 @@
 package interview_preparation_kit.arrays
 
-import java.util.*
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.ranges.*
+import kotlin.text.*
 
 /**
- * Challenge:
-Starting with a 1-indexed array of zeros and a list of operations,
-for each operation add a value to each the array element between two given indices,
-inclusive. Once all operations have been performed,
-return the maximum value in the array.
-
- Support resource for solution: https://stackoverflow.com/questions/56249944/array-manipulation-hackerrank-questions-java
- including explanation of steps to avoid timeout exception.
-
- IN PROGRESS, failing 10/16 tests. Larger arrays fail.
- **/
-
-/*
  * Complete the 'arrayManipulation' function below.
  *
  * The function is expected to return a LONG_INTEGER.
@@ -24,29 +14,35 @@ return the maximum value in the array.
  *  2. 2D_INTEGER_ARRAY queries
  */
 
-fun arrayManipulation(n: Int, queries: Array<IntArray>): Long {
-    var max = 0L
-    var a: Int
-    var b: Int
-    var k: Int
-    val arr = IntArray(n + 2)
+fun arrayManipulation(n: Int, queries: Array<Array<Int>>): Long {
+    // Write your code here
+
+    val outputArray = LongArray(n + 2)
     for (i in queries.indices) {
-        a = queries[i][0]
-        b = queries[i][1]
-        k = queries[i][2]
-        for (j in arr.indices) {
-            if (j >= a) {
-                arr[j] = arr[j] + k
-            }
-            if (j > b) {
-                arr[j] = arr[j] - k
-            }
-        }
+        val a = queries[i][0]
+        val b = queries[i][1]
+        val k = queries[i][2]
+        outputArray[a] += k.toLong()
+        outputArray[b + 1] -= k.toLong()
     }
-    Arrays.sort(arr)
-    max = arr[arr.size - 1].toLong()
+    return getMax(outputArray)
+}
+
+/**
+ * @param inputArray
+ * @return
+ */
+private fun getMax(inputArray: LongArray): Long {
+    var max = Long.MIN_VALUE
+    var sum: Long = 0
+    for (i in inputArray.indices) {
+        sum += inputArray[i]
+        max = Math.max(max, sum)
+    }
     return max
 }
+
+
 
 fun main(args: Array<String>) {
     val first_multiple_input = readLine()!!.trimEnd().split(" ")
@@ -61,41 +57,7 @@ fun main(args: Array<String>) {
         queries[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
     }
 
-    /**
-     * Commented out to get rid of error in file
-     */
-   // val result = arrayManipulation(n, queries)
+    val result = arrayManipulation(n, queries)
 
-  //  println(result)
+    println(result)
 }
-
-/*
-This solution returned a time out error when checking the code on the site
- */
-//fun arrayManipulation(n: Int, queries: Array<Array<Int>>): Long {
-//    // Write your code here
-//    //create array of 0s of size n (https://stackoverflow.com/questions/31366229/how-to-initialize-an-array-in-kotlin-with-values)
-//    val arr = IntArray(n) {0}
-//    var currentVal = 0
-//    var maxVal = 0
-//    var i = 0
-//
-//    queries.forEach {
-//        val startRange = it[0]+1
-//        val endRange = it[1]+1
-//        val value = it[2]
-//
-//        while (i < n) {
-//            arr[startRange]+=value
-//            arr[endRange]-=value
-//            currentVal = arr.toList().max()!!
-//        }
-//
-//        if (currentVal > maxVal) {
-//            maxVal = currentVal
-//        }
-//    }
-//
-//    return maxVal.toLong()
-//
-//}
